3cbe4079b6a394ce602d098abe4d72b022967c27
Jeeshan5 <mo.jeeshan1133@gmail.com>
2025-05-05 19:01:34 +0530

    fixed topbar

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -2,41 +2,34 @@ import { useState } from "react";
 import "./topbar.css";
 import hamburgerIcon from "../assets/Hamburger_icon.svg.png";
 
-
-
 export default function Topbar() {
   const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
 
   return (
     <div className="top">
       <div className="topLeft">
-        <i className="topIcon fa-brands fa-facebook fa-2x"></i>
-        <i className="topIcon fa-brands fa-x-twitter fa-2x"></i>
-        <i className="topIcon fa-brands fa-pinterest-p fa-2x"></i>
-        <i className="topIcon fa-brands fa-instagram fa-2x"></i>
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-x-twitter"></i>
+        <i className="topIcon fa-brands fa-pinterest-p"></i>
+        <i className="topIcon fa-brands fa-instagram"></i>
       </div>
 
-      {/* Hamburger Icon */}
-      <div className="menuToggle" onClick={handleToggle}>
+      <div className="menuToggle" onClick={() => setMenuOpen(!menuOpen)}>
         <img src={hamburgerIcon} alt="Menu" className="hamburgerIcon" />
       </div>
 
       <div className={`topCenter ${menuOpen ? "show" : ""}`}>
-  <ul className={`topList ${menuOpen ? "show" : ""}`}>
-    <li className="topListItem">Home</li>
-    <li className="topListItem">About</li>
-    <li className="topListItem">Contact</li>
-    <li className="topListItem">Write</li>
-  </ul>
-</div>
+        <ul className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Write</li>
+        </ul>
+      </div>
 
       <div className="topRight">
         <img className="topImg" src="" alt="profile" />
-        <i className="topSearchIcon fa-solid fa-magnifying-glass fa-2x"></i>
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
   );
